1c098764a070c53a7ad3343f491503d37277c71f
tberry147 <totimab@yahoo.com>
2022-06-21 21:28:43 -0400

    delete local.vpc

--- sg.tf
+++ sg.tf
@@ -2,8 +2,7 @@
 resource "aws_security_group" "jenkins" {
   name        = "jenkins"
   description = "Allow 8080"
-  vpc_id      = local.vpc_id
-
+  
   ingress {
     description     = "allow ${var.jenkins_port}"
     from_port       = var.jenkins_port
